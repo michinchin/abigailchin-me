9bf723e8a45079cb4438f5e22c039e3edc03fd90
michinchin <chinabigail08@yahoo.com>
2022-12-10 23:01:51 -0800

    initial route

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App/>,
+  errorElement: <ErrorPage/>
+}]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
